6b0f82956b7500e084fec9ef128090a688d5fb9f
anasalurb <ana.su.1995@hotmail.com>
2016-02-27 18:31:19 +0100

    third version ana

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var members=[];
 var app=express();
 
 app.get("/about/spain-births",(req,res)=>{
-	fs.readFile('spain-births.json','utf8',(err,content)=>{
+	fs.readFile('spain-births.json','utf-8',(err,content)=>{
 		console.log("Data read");
 		births=JSON.parse(content);
 		res.write('<html><body>It will display data from spanish births, by regions and years,<br /> making difference between men and women, showing the total number in the last column.<br />');
@@ -20,11 +20,11 @@ app.get("/about/spain-births",(req,res)=>{
 });
 
 app.get("/about/population-growth",(req,res)=>{
-	fs.readFile('population-growth.json','utf8',(err,content)=>{
+	fs.readFile('population-growth.json','utf-8',(err,content)=>{
 		console.log("Data read");
 		pops=JSON.parse(content);
 		res.write('<html><h2>Population growth</h2>');
-		res.write('<body>This page shows data about population growth in Spain in the lastest years. <br /> Data are classified acoording to differents parameters, such as: <br /><ul>');
+		res.write('<body>This page shows data about population growth in Spain in the lastest years. <br /> Data are classified acording to differents parameters, such as: <br /><ul>');
 		res.write('<li>Region</li>');
 		res.write('<li>Year</li>');
 		res.write('<li>Age</li>');
@@ -43,7 +43,7 @@ app.get("/about/population-growth",(req,res)=>{
 });
 
 app.get('/about',(req,res) => {
-	fs.readFile('members.json','utf8',(err,content) => {
+	fs.readFile('members.json','utf-8',(err,content) => {
 		console.log("Data read");
 		members=JSON.parse(content);
 		res.write("<html><body>Group members:<ul>");
