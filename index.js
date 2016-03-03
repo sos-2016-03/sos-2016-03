8a0610a488238a3ea4b2e1510664d5e324028429
anasalurb <ana.su.1995@hotmail.com>
2016-03-03 18:28:10 +0100

    D1.2 ana second version

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 var express=require("express");
 var fs=require("fs");
 var app=express();
-/*
-app.get("/about/population-growth",(req,res)=>{
-	fs.readFile('population-growth.json','utf-8',(err,content)=>{
-		console.log("Data read");
-		pops=JSON.parse(content);
-		res.write('<html><h2>Population growth</h2>');
-		res.write('<body>This page shows data about population growth in Spain in the lastest years. <br /> Data are classified acording to differents parameters, such as: <br /><ul>');
-		res.write('<li>Region</li>');
-		res.write('<li>Year</li>');
-		res.write('<li>Age</li>');
-		res.write('<li>Men</li>');
-		res.write('<li>Women</li>');
-		res.write('<li>Total population</li>');
-		res.write("</ul>Data example:<ul>");
-		res.write("<li>region, year => men, women, total population</li>")
-		pops.forEach((pop) =>{
-			res.write("<li>"+pop.region+", "+pop.year+" => "+pop.men+", "+pop.women+", "+pop.totalpopulation+"</li>");
-		});
-		res.write("</body></html>");
-		res.end();
-	});
-	
-});*/
+
 
 app.get("/about/mort-sickness",(req,res)=>{
 	fs.readFile('mort-sickness.json', 'utf8', (err, content)=>{
