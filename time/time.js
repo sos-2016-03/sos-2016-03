09a40751a26701b367d617d2ec8338734697a8c1
patricia28 <patrigonzalez28@gmail.com>
2016-03-04 12:19:25 +0100

    Time

--- time/time.js
+++ time/time.js
@@ -2,9 +2,14 @@ var express = require("express");
 var app = express();
 app.use("/",express.static(__dirname + '/time'));
 
+var port = (process.env.PORT || 5000);
+
 app.get("/time",(req,res)=>{
 	var now = new Date();
 	res.write("It is: " + now);
 	res.end();
 });
+
+
+app.listen(port);
 
