bfe89fe4c33751d34fc52f3919818c8401161c12
patricia28 <patrigonzalez28@gmail.com>
2016-03-04 21:38:50 +0100

    Time

--- time/time.js
+++ time/time.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var app = express();
-app.use("/time",express.static(__dirname + '/time'));
 
-app.get("/time",(req,res)=>{
+app.get("/",(req,res)=>{
 	var now = new Date();
 	res.write("It is: " + now);
 	res.end();
