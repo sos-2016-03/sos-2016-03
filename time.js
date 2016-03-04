fb99ec05c4b4cf7a2a2f98e6f5c58905abcb424b
patricia28 <patrigonzalez28@gmail.com>
2016-03-04 21:33:48 +0100

    Time

--- time.js
+++ time.js
@@ -1,10 +1,9 @@
 var express = require("express");
+var app = express();
 
-var app = require("fs");
 
 app.get("/time",(req,res)=>{
 	var now = new Date();
 	res.write("It is: " + now);
 	res.end();
 });
-
