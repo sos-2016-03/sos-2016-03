c526b6bbbb29054b0e0a48e39a7ca0ea93c670d4
albrodpul <albrodpul@gmail.com>
2016-03-04 09:49:01 +0100

    D1.1

--- index.js
+++ index.js
@@ -19,6 +19,59 @@ app.get("/about/mort-sickness",(req,res)=>{
 	});
 });
 
+app.get"/about",(req,res)=>{
+	fs.readFile('members.json','utf8',(err,content)=>{
+		members=JSON.parse(content);
+  		members.forEach((member) =>{
+  			res.write("<html><body><h3>Group members:</h3><ul>");
+ 			res.write("<li>"+member.name+" => <a href=/about/"+member.source+">"+member.source+"</a></li>");
+ 		});
+		res.write("</ul><h3>Project theme:</h3>");
+		res.write("<p style='text-align: justify;'>Our sources of information are aimed for analyzing the relationship between the number of births over the years in the regions of Spain, along with the number of deaths due to different types of disease and population growth in our country.</p></body></html>");
+ 		res.end();
+ 	});
+ });
+
+app.get("/about/spain-births",(req,res)=>{
+  	fs.readFile('spain-births.json','utf-8',(err,content)=>{
+ 		console.log("Data read");
+ 		births=JSON.parse(content);
+		res.write('<html><body><h3>Spanish births </h3>');
+ 		res.write("<p style='text-align: justify;'>It will display data about spanish births, by regions and years. Making difference between men and women, showing the total number of births in the last column, as following: </p>");		
+ 		res.write("Data example:<ul>");
+ 		res.write("<li>region, year => men, women, total birth</li>");
+ 		births.forEach((birth) =>{
+ 			res.write("<li>"+birth.region+", "+birth.year+" => "+birth.men+", "+birth.women+", "+birth.totalbirth+"</li>");
+ 		});
+ 		res.write("</body></html>");
+ 		res.end();
+ 	});
+ 	
+ });
+
+app.get("/about/population-growth",(req,res)=>{
+ 	fs.readFile('population-growth.json','utf-8',(err,content)=>{
+ 		console.log("Data read");
+ 		pops=JSON.parse(content);
+ 		res.write('<html><h2>Population growth</h2>');
+ 		res.write('<body>This page shows data about population growth in Spain in the lastest years. <br /> Data are classified acording to differents parameters, such as: <br /><ul>');
+ 		res.write('<li>Region</li>');
+ 		res.write('<li>Year</li>');
+ 		res.write('<li>Age</li>');
+ 		res.write('<li>Men</li>');
+ 		res.write('<li>Women</li>');
+ 		res.write('<li>Total population</li>');
+ 		res.write("</ul>Data example:<ul>");
+ 		res.write("<li>region, year => men, women, total population</li>")
+ 		pops.forEach((pop) =>{
+ 			res.write("<li>"+pop.region+", "+pop.year+" => "+pop.men+", "+pop.women+", "+pop.totalpopulation+"</li>");
+ 		});
+ 		res.write("</body></html>");
+ 		res.end();
+ 	});
+ 	
+ }); 	
+
 app.use('/',express.static(__dirname + '/public'));
 app.use('/about',express.static(__dirname + '/about'));
 
