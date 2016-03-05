2240fba387643feab1576d37875ee88bcc6565cf
patricia28 <patrigonzalez28@gmail.com>
2016-03-05 09:44:06 +0100

    Second version

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/about/mort-sickness",(req,res)=>{
 		sic = JSON.parse(content);
 		res.write('<html><h1>Mort sickness</h1>');
 		res.write('<body>My data source is about mortality sexually transmited disease. For example:</br>');
-		res.write("<table><tr><td>"region"</td>, <td>"year"</td>, <td>"mortality-in-men"</td>, <td>"mortality-in-women"</td>, <td>"total-mortality"</td></tr></table>");
+		res.write("<table><tr><td>region</td>, <td>year</td>, <td>mortality-in-men</td>, <td>mortality-in-women</td>, <td>total-mortality</td></tr>");
 		sic.forEach((sick) =>{
 			res.write("<li>" + sick.region + " " +sick.sickness + " " + sick.year + " " + sick.mortalityinmen  + " " + sick.mortalityinwomen + " "+ sick.totalmortality + "</li>");
 		});
