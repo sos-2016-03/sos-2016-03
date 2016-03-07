da2c313ea4db39a9b6dfebaaf404edfa31cb1bac
albrodpul <albrodpul@gmail.com>
2016-03-07 16:29:31 +0100

    time fix

--- index.js
+++ index.js
@@ -4,39 +4,31 @@ var app=express();
 
 
 app.get("/time",(req,res)=>{
-	var days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+	var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	var months=[
   		"January", "February", "March",
 		  "April", "May", "June", "July",
 		  "August", "September", "October",
 		  "November", "December"
 		  ];
-	var now = new Date();
-	var hour=now.getHours()+1;
-	var min=now.getMinutes();
-	var sec=now.getSeconds();
-	var year=now.getFullYear();
-	var month=now.getMonth();
-	var day=now.getDate();
+	var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+	var year=date.getFullYear();
+	var month=date.getMonth();
+	var day=date.getDay();
+	var weekday=date.getDate();
 	res.write("<html><head><title>Group 03 - My time</title><body>");
 	if(hour<12){
-		if(sec<10){
-			res.write("<h2>Good morning, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":0"+sec+"</h2><br />");
-		}else{
-			res.write("<h2>Good morning, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
-		}
+		res.write("<h2>Good morning, today is "+days[day]+" "+weekday+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
 	}else if(hour<20){
-		if(sec<10){
-			res.write("<h2>Good afternoon, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":0"+sec+"</h2><br />");
-		}else{
-			res.write("<h2>Good afternoon, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
-		}
+		res.write("<h2>Good afternoon, today is "+days[day]+" "+weekday+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
 	}else{
-		if(sec<10){
-			res.write("<h2>Good night, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":0"+sec+"</h2><br />");
-		}else{
-			res.write("<h2>Good night, today is "+days[day]+" "+day+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
-		}
+		res.write("<h2>Good night, today is "+days[day]+" "+weekday+" "+months[month]+" "+year+" and it is "+hour+":"+min+":"+sec+"</h2><br />");
 	}
 	res.write("<a href='/about/'>« Previous</a></body></html>");
 	res.end();
@@ -46,7 +38,6 @@ app.get("/time",(req,res)=>{
 
 app.use('/',express.static(__dirname + '/public'));
 app.use('/about',express.static(__dirname + '/about'));
-
 
 app.listen(process.env.PORT || 11000);
 
