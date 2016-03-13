1ed41c8e470953606fe7908d99fcf0723e0e876c
patricia28 <patrigonzalez28@gmail.com>
2016-03-13 13:16:07 +0100

    Last version. Closes #33

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app=express();
 
 var bodyParser=require("body-parser");
 
-
 app.get("/time",(req,res)=>{
 	var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	var months=[
@@ -38,17 +37,98 @@ app.get("/time",(req,res)=>{
 
 app.use(bodyParser.json());
 
-app.post("/music",function(req,res){
+var musics=[];
+
+app.post("/api/sandbox/music",(req,res)=>{
+	var music = req.body;
+	musics.push(music);
+	console.log("New POST " + music.name);
 	res.sendStatus(200);
-	console.log("New POST");
-	console.log("Object received: " + JSON.stringify(req.body));
-	music.push(req.body);
 });
 
+app.post("/api/sandbox/music/:name", (req,res)=>{
+	console.log("Method not allowed");
+	res.sendStatus(405);
+});
 
+app.get("/api/sandbox/music",(req,res)=>{
+	console.log("NEW GET");
+	var music = req.body;
+	res.send(musics);
+});
+
+app.get("/api/sandbox/music/:name", (req,res)=>{
+	var buscado = -1;
+	var name = req.params.name;
+	for(var i=0;i<musics.length;i++){
+		if(name==musics[i].name){
+			buscado = i;
+			console.log("New GET of resource name");
+			var atributo = musics[i];
+			res.send(atributo);
+			break;	
+		}
+	}
+	if(buscado == -1){
+		console.log("GET Not Found");
+		res.sendStatus(404);
+	}
+});
+
+app.delete("/api/sandbox/music",(req,res)=>{
+	for(var i=0; i<musics.length; i++){
+		musics.splice(i);
+		res.send(200);
+	}
+	console.log("Delete songs list");
+});
+
+app.delete("/api/sandbox/music/:name",(req,res)=>{
+	var buscado = -1;
+	var name = req.params.name;
+	for(var i=0; i<musics.length; i++){
+		if(name==musics[i].name){
+		buscado = i;
+		console.log("Delete this song");
+		musics.splice(i);
+		res.sendStatus(200);
+		}	
+	}
+	if(buscado == -1){
+		res.sendStatus(404);
+	}
+});
+
+app.put("/api/sandbox/music", (req,res)=>{
+	console.log("Method not allowed");
+	res.sendStatus(405);
+});
+
+app.put("/api/sandbox/music/:name", (req,res)=>{
+	var buscado = -1;
+	var name = req.params.name;
+	var nueva = req.body;
+	for(var i=0; i<musics.length; i++){
+		if(name==musics[i].name){
+			buscado=i;
+			musics[i]=nueva;
+			console.log("Modified");
+			res.sendStatus(200);
+		}
+	}
+	if(buscado == -1){
+		res.sendStatus(404);
+	}
+});
+
+app.get("/api-test/music/loadInitialData", (req,res)=>{
+	musics=[{"name ": "Frio", "author ": "Maria Parrado"},
+	{"name ": "Te dejo en libertad", "author ": "Ha-Ash"}]
+	res.send(musics);
+	console.log("This songs are my initial data");
+});
 
 app.use('/',express.static(__dirname + '/public'));
-
 
 app.listen(process.env.PORT || 11000);
 
