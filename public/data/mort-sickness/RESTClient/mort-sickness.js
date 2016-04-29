49bfec238308c2db9a13bb73e793db80d81ec2bd
patricia28 <patrigonzalez28@gmail.com>
2016-04-29 12:04:19 +0200

    Introduction

---
+++ public/data/mort-sickness/RESTClient/mort-sickness.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('#url').jqxInput({
+		theme:'orange'
+	})
+});
+
+$(document).ready(function(){
+	$('#payload').jqxInput({
+		theme:'orange'
+	})
+});
+
+$(document).ready(function(){
+	$('#button').jqxButton({
+		theme:'shinyblack'
+	})
+});
+
+$(document).ready(function(){
+	$('#miBoton').jqxButton({
+		theme:'shinyblack'
+	})
+});
---
+++ public/data/mort-sickness/mort-sickness.js
@@ -0,0 +1,39 @@
+var service = '../../api/v1/';
+$(document).ready(function(){
+	jQuery.support.cors=true;
+	$.ajax({
+		type:"GET",
+		url:service + 'mort-sickness?apikey=patriR',
+		data:"{}",
+		contentType:"application/json; charset=utf-8",
+		dataType:"json",
+		cache:false,
+		success: function(data){
+			var trHTML = '';
+			$.each(data, function(i,item){
+				trHTML += '<tr><td>' + data[i].region + '</td><td>' + data[i].sickness + '</td><td>' + data[i].year + '</td><td>' + data[i].mortalityInMen + '</td><td>' + data[i].mortalityInWomen + '</td><td>' + data[i].totalMortality + '</td></tr>';
+			});
+			$('#sickness').append(trHTML);
+			$('#sickness').jqxDataTable({
+				altRows: true,
+				theme: 'orange',
+				columns: [
+                  { text: 'Region', dataField: 'Region', width: 220},
+                  { text: 'Sickness', dataField: 'Sickness', width: 220},
+                  { text: 'Year', dataField: 'Year', width: 220},
+                  { text: 'MortalityInMen', dataField: 'MortalityInMen',width: 220},
+                  { text: 'MortalityInWomen', dataField: 'MortalityInWomen', width: 220},
+                  { text: 'TotalMortality', dataField: 'TotalMortality', width: 220}
+                ]
+			})
+		},
+		error: function(msg){
+			alert(msg.responseText);
+		}
+	})
+});
+$(document).ready(function(){
+	$('#miBoton').jqxButton({
+		theme: 'shinyblack'
+	})
+});
