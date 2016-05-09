8480256e50080be22c892b81442091d734b1fb1f
patricia28 <patrigonzalez28@gmail.com>
2016-05-09 23:48:37 +0200

    Deliverable D03.2

--- public/mort-sickness/mort-sickness.js
+++ public/mort-sickness/mort-sickness.js
@@ -6,9 +6,13 @@ function verTodos(){
     console.log("JQuery Ready!");
     $("#GET").click(function(){     
       $("#log").text("Sending request...");
+      var dir="";
+      if($("#limit").val()!='' && $("#offset").val()!=''){
+        dir=service + "?apikey=" + $("#apikey").val() + "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1);
+      }
 
       var request = $.ajax({
-        url: service + "?apikey=" + $("#apikey").val() + "&limit=" + $("#limit").val() + "&offset=" + $("#offset").val(),
+        url: dir,
         type: "GET",
         data: "{}",
         contentType: "application/json"
@@ -25,12 +29,12 @@ function verTodos(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text( "Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       });
        function imprime(data){
@@ -73,14 +77,15 @@ $('#miBotonInicial').click(function(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text("Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       });
+  
 })
 };
 
@@ -105,16 +110,16 @@ function nuevo(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text("Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }else if(statusCode == 400){
-            	$("#status").text(statusCode + " " + "Faltan parámetros");
+            	$("#status").text("Missing parameters");
             }else if(statusCode == 409){
-            	$("#status").text(statusCode + " " + "Ya existe el dato");
+            	$("#status").text("Data exist");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       })     
 }
@@ -140,16 +145,16 @@ function actualizar(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text("Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }else if(statusCode == 400){
-            	$("#status").text(statusCode + " " + "Faltan parámetros");
+            	$("#status").text("Missing parameters");
             }else if(statusCode == 404){
-            	$("#status").text(statusCode + " " + "El dato no existe");
+            	$("#status").text("Data not exist");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       })   
 }
@@ -174,14 +179,14 @@ function eliminarDato(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text("Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }else if(statusCode == 404){
-            	$("#status").text(statusCode + " " + "No existe el dato");
+            	$("#status").text("Data not exist");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       });    
 }
@@ -208,38 +213,48 @@ function eliminarTodo(){
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
         	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
+            	$("#status").text("Insert a correct apikey");
         	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+        		$("#status").text("Incorrect apikey");
             }
         }else{
-            $("#status").text(status);
+            $("#status").text("OK");
         }
       })
       function imprime(data){
        	$.each(data,function(i,item){
           $('#sickness').empty();
-          var trHTML = '';
+          /*var trHTML = '';
                trHTML += '<tr bgcolor="orange"><th>Region</th><th>Sickness</th><th>Year</th><th>MortalityInMen</th><th>MortalityInWomen</th><th>TotalMortality</th></tr>';
              
-                 $('#sickness').append(trHTML); 
+                 $('#sickness').append(trHTML); */
                })
             }
 }
 
-//Busca por región
-function buscaPorRegion(){
-    console.log("JQuery Ready!");
-    $("#buscaRegion1").click(function(){     
-      $("#log").text("Sending request...");
+//Busqueda
+function buscar(){
+  var dir;
+  if($("#region1").val()!=''  && $("#from").val()=='' && $("#to").val()==''){
+    dir= service + "/" + $("#region1").val() + "/" + "?apikey=" + $("#apikey").val()+ "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1);
+  }else if($("#region1").val()=='' && $("#from").val()=='' && $("#to").val()==''){
+    dir= service + "/" + "?apikey=" + $("#apikey").val()+ "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1);
+  }else if($("#region1").val()=='' && $("#from").val()!='' && $("#to").val()==''){
+    dir= service + "/" + "?apikey=" + $("#apikey").val()+ "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1) + "&from=" + $("#from").val(); 
+  }else if($("#region1").val()==''  && $("#from").val()!='' && $("#to").val()!=''){
+    dir= service + "/" + "?apikey=" + $("#apikey").val()+ "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1) + "&from=" + $("#from").val() + "&to=" + $("#to").val(); 
+  }else if($("#region1").val()!=''&& $("#from").val()!='' && $("#to").val()!=''){
+    dir= service + "/" + $("#region1").val() + "/" + "?apikey=" + $("#apikey").val()+ "&limit=" + $("#limit").val() + "&offset=" + $("#limit").val()*($("#offset").val()-1) + "&from=" + $("#from").val() + "&to=" + $("#to").val(); 
+  }
 
       var request = $.ajax({
-        url: service + "/" +  $("#region1").val() + "/" +"?apikey=" + $("#apikey").val() + "&limit=" + $("#limit").val() + "&offset=" + $("#offset").val(),
+        url: dir,
         type: "GET",
         data: "{}",
         contentType: "application/json"
       });
-      request.done(function(data,status,jqXHR){       
+
+      request.done(function(data,status,jqXHR){      
         var statusCode = jqXHR.status;
         var statusCodeText = jqXHR.statusText;
         $("#log").text("Data received");
@@ -250,73 +265,19 @@ function buscaPorRegion(){
         var statusCode = jqXHR.status;
         var statusCodeText = jqXHR.statusText;
         if (status == "error"){
-        	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
-        	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
-            }else if(statusCode == 404){
-            	$("#status").text(statusCode + " " + "No existe el dato");
-            }
-        }else{
-            $("#txtStatus").text(status);
-        }
-      });
-       function imprime(data){
-       	$.each(data,function(i,item){
-          $('#sickness').empty();
-          var trHTML = '';
-               trHTML += '<tr bgcolor="orange"><th>Region</th><th>Sickness</th><th>Year</th><th>MortalityInMen</th><th>MortalityInWomen</th><th>TotalMortality</th></tr>';
-                    $.each(data, function(i,item){
-                    trHTML += '<tr><td>' + 
-                    data[i].region + '</td><td>' + 
-                    data[i].sickness + '</td><td>' + 
-                    data[i].year + '</td><td>' + 
-                    data[i].mortalityInMen + '</td><td>' + 
-                    data[i].mortalityInWomen + '</td><td>' +
-                    data[i].totalMortality + '</td></tr>'; 
-                    });
-                 $('#sickness').append(trHTML); 
-               })
-            }
-        })    
-    }
-
-//Busca por año
-function buscaPorAño(){
-    console.log("JQuery Ready!");
-    $("#buscaAño1").click(function(){     
-      $("#log").text("Sending request...");
-
-      var request = $.ajax({
-        url: service + "/" +  $("#year1").val() + "/" +"?apikey=" + $("#apikey").val() + "&limit=" + $("#limit").val() + "&offset=" + $("#offset").val(),
-        type: "GET",
-        data: "{}",
-        contentType: "application/json"
-      });
-      request.done(function(data,status,jqXHR){       
-        var statusCode = jqXHR.status;
-        var statusCodeText = jqXHR.statusText;
-        $("#log").text("Data received");
-        $("#status").text(statusCode +" "+statusCodeText);
-        $("#sickness").html(imprime(data));
-      });
-      request.always(function(jqXHR, status){
-        var statusCode = jqXHR.status;
-        var statusCodeText = jqXHR.statusText;
-        if (status == "error"){
-        	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
-        	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
+          if(statusCode == 401){
+              $("#status").text("Insert a correct apikey");
+          }else if(statusCode == 403){
+            $("#status").text("Incorrect apikey");
             }else if(statusCode==404){
-            	$("#status").text(statusCode + " " + "No existe el dato");
+              $("#status").text("Data not exist");
             }
         }else{
-            $("#txtStatus").text(status);
+            $("#status").text("OK");
         }
       });
        function imprime(data){
-       	$.each(data,function(i,item){
+        $.each(data,function(i,item){
           $('#sickness').empty();
           var trHTML = '';
                trHTML += '<tr bgcolor="orange"><th>Region</th><th>Sickness</th><th>Year</th><th>MortalityInMen</th><th>MortalityInWomen</th><th>TotalMortality</th></tr>';
@@ -331,66 +292,46 @@ function buscaPorAño(){
                     });
                  $('#sickness').append(trHTML); 
                })
-            }
-        })    
+            }    
     }
 
-//Busca entre años
-function buscaEntreAños(){
-    console.log("JQuery Ready!");
-    $("#entreAños").click(function(){     
-      $("#log").text("Sending request...");
-
-      var request = $.ajax({
-        url: service + "/" +"?apikey=" + $("#apikey").val() + "&limit=" + $("#limit").val() + "&offset=" + $("#offset").val() + "&from=" + $("#from").val() + "&to=" + $("#to").val(),
+function pagination(){
+    var request= $.ajax(
+    {
         type: "GET",
+        url: service + '?apikey=' + $("#apikey").val() + '&limit='+ $("#limit").val() +'&offset='+ $("#limit").val()*($("#offset").val()-1),
         data: "{}",
-        contentType: "application/json"
-      });
-      request.done(function(data,status,jqXHR){       
-        var statusCode = jqXHR.status;
-        var statusCodeText = jqXHR.statusText;
-        $("#log").text("Data received");
-        $("#status").text(statusCode +" "+statusCodeText);
-        $("#sickness").html(imprime(data));
-      });
-      request.always(function(jqXHR, status){
-        var statusCode = jqXHR.status;
-        var statusCodeText = jqXHR.statusText;
-        if (status == "error"){
-        	if(statusCode == 401){
-            	$("#status").text(statusCode + " " + "Insert a correct apikey");
-        	}else if(statusCode == 403){
-        		$("#status").text(statusCode + " " + "Incorrect apikey");
-            }else if(statusCode==404){
-            	$("#status").text(statusCode + " " + "No existe el dato");
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        cache: false,
+      })
+    request.always(function(jqXHR,status,error){
+          if(error){
+            if(jqXHR.status == 401){
+                $("#status").text("Insert an apikey");
+            }else if(jqXHR.status == 403){
+                $("#status").text("Incorrect apikey");
+            }else if(jqXHR.status == 404){
+               $("#status").text("Data not found");
             }
-        }else{
-            $("#txtStatus").text(status);
-        }
-      });
-       function imprime(data){
-       	$.each(data,function(i,item){
-          $('#sickness').empty();
-          var trHTML = '';
-               trHTML += '<tr bgcolor="orange"><th>Region</th><th>Sickness</th><th>Year</th><th>MortalityInMen</th><th>MortalityInWomen</th><th>TotalMortality</th></tr>';
-                    $.each(data, function(i,item){
-                    trHTML += '<tr><td>' + 
-                    data[i].region + '</td><td>' + 
-                    data[i].sickness + '</td><td>' + 
-                    data[i].year + '</td><td>' + 
-                    data[i].mortalityInMen + '</td><td>' + 
-                    data[i].mortalityInWomen + '</td><td>' +
-                    data[i].totalMortality + '</td></tr>'; 
-                    });
-                 $('#sickness').append(trHTML); 
-               })
+          }else if(jqXHR.status == 200){
+               $("#status").text("OK");
             }
-        })    
-    }
-
-
-
+        })
+     request.done(function (data) {
+       $.each(data, function (i, item) {
+          var trHTML='';
+         if($("#status").val()==404){
+          trHTML="";
+         }else{
+          console.log(data[i]);
+          trHTML= "<tr><td>" + data[i].region + '</td><td>' + data[i].sickness + '</td><td>' + data[i].year + '</td><td>' + data[i].mortalityInMen + '</td><td>' + data[i].mortalityInWomen + '</td><td>' + data[i].totalMortality + '</td></tr>';
+          }
+          
+          })
+        })
+    
+}
 
 //Estilo
 $(document).ready(function(){
@@ -416,19 +357,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$('#buscaRegion1').jqxButton({
-		theme:'darkblue'
-	})
-});
-
-$(document).ready(function(){
-	$('#buscaAño1').jqxButton({
-		theme:'darkblue'
-	})
-});
-
-$(document).ready(function(){
-	$('#entreAños').jqxButton({
+	$('#buscar').jqxButton({
 		theme:'darkblue'
 	})
 });
