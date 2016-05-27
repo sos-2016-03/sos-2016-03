2d9003c091b0e763f52687fd0c398f86d3bfb3f8
patricia28 <patrigonzalez28@gmail.com>
2016-05-28 01:54:19 +0200

    D03.C

--- public/integration/Patricia/charts/chart4.js
+++ public/integration/Patricia/charts/chart4.js
@@ -1,26 +1,23 @@
-google.charts.load("current", {packages: ["corechart"]});
 google.charts.setOnLoadCallback(drawVisualization);
 
 //Consumo de una api externa 
 
 function drawVisualization(){
-  $(document).ready(function(d){
-    var p=/^http:/.test(d.location)?'http':'https';
+  $(document).ready(function(){
     var request=$.ajax({
-      url:p + "://datos.santander.es/api/rest/collections.json"
+      url:"http://datos.alcobendas.org/dataset/39039ceb-67fe-42f8-bd51-82ed9834945d/resource/efc48843-fc49-49ba-b249-50784e0fbbc6/download/presupuestoconsolidadodegastos.json/"
     })
     request.done(function(data, status){
-        var datos = [["Identifier","Size"]];
-        for(i=0;i<data.collections.identifier.length;i++){
-            graf=data.collections.identifier.length[i];
-            graf1 = data.collections.size.length[i];
-            var grafForWidget=[graf, graf1];
+        var datos = [["Denomination","Total expenses"]];
+        for(i=0;i<data.length;i++){
+            graf=data[i];
+            var grafForWidget=[graf.denominacion, graf.total];
             datos.push(grafForWidget);
           
         }
         var datosRecogidos = google.visualization.arrayToDataTable(datos);
         var options = {
-              title: 'Consume external api of complete list of directory resources',
+              title: 'Consume external api of total expenses of Alcobendas in 2013',
               pieHole: 0.4,
            };
           var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
